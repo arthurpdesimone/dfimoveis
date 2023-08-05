abe1d438c6a133bd248eb1b5110885b111af5652
arthurpdesimone <arthurpdesimone@gmail.com>
2023-08-05 13:13:37 -0300

    Version 0.2: Google Maps

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 '''
 @author: Arthur Pendragon De Simone
 '''
+from datetime import datetime
+import googlemaps as googlemaps
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='templates/assets')
@@ -11,8 +13,15 @@ def index():
     return render_template('index.html')
 
 @app.route('/<api>',methods=['GET'])
-def download_imoveis(api):
-    return f"Hello from flask{api}"
+def download_imoveis(api_key):
+    gmaps = googlemaps.Client(key=api_key)
+    carro_direcoes = gmaps.directions("Rua Dias De Oliveira, 192",
+                                      "Rua Vespasiano, 76",
+                                      mode="driving",
+                                      avoid="ferries",
+                                      departure_time= datetime.now()
+                                      )
+    return carro_direcoes[0]
 
 if __name__ == "__main__":
  app.run()
